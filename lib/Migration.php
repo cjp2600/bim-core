d2c5a293bf76aac306304ecf52026fe43e9e108d
Stanislav <cjp363879>
2015-01-22 08:15:37 +0300

    add new file

---
+++ lib/Migration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created for the project "bim-core"
+ *
+ * @author: Stanislav Semenov (CJP2600)
+ * @email: cjp2600@ya.ru
+ *
+ * @date: 22.01.2015
+ * @time: 7:48
+ */
+
+namespace Bim;
+
+use ConsoleKit\Console;
+
+class Migration {
+
+    public static function init()
+    {
+        $conf = new \Noodlehaus\Config(__DIR__."/conf.json");
+        $console = new Console($conf->get("commands"));
+        $console->run();
+    }
+}
