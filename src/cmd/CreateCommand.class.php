4251831ca407df148a2bc4f8fc56873fb8086a7f
Stanislav <cjp363879>
2015-01-24 13:03:03 +0300

    add create other command

--- src/cmd/CreateCommand.class.php
+++ src/cmd/CreateCommand.class.php
@@ -5,7 +5,8 @@
  * Main class of creating migration is required for the generation of migration files.
  *
  * The following commands create:
- *   - Iblock # Create a migration of information blocks
+ *   - iblock
+ *   - other
  *
  */
 class CreateCommand extends BaseCommand {
@@ -20,7 +21,7 @@ class CreateCommand extends BaseCommand {
                 throw new Exception("Missing command, see help Example: php bim help create");
             }
         } else {
-            throw new Exception("Required command is empty, see help Example: php bim help create");
+            $this->createOther($args,$options);
         }
     }
 
@@ -48,6 +49,32 @@ class CreateCommand extends BaseCommand {
                 get_current_user()
         ));
 
+    }
+
+
+    /**
+     * createOther
+     * @param array $args
+     * @param array $options
+     */
+    public function createOther(array $args, array $options = array())
+    {
+        # get description options
+        $desc = (isset($options['d'])) ? $options['d'] : "";
+
+        $up_data = array();
+        $down_data = array();
+
+        # set
+        $name_migration = $this->getMigrationName($this->fromCamelCase(__METHOD__));
+        $this->saveTemplate($name_migration,
+            $this->setTemplate(
+                $name_migration,
+                $this->setTemplateMethod(strtolower($args[0]),$up_data),
+                $this->setTemplateMethod(strtolower($args[0]),$down_data,"down"),
+                $desc,
+                get_current_user()
+            ));
     }
 
 }
