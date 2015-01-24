cd6cac80534ab5fe94ee9f5080ada4d6f294dedf
Stanislav <cjp363879>
2015-01-24 18:02:03 +0300

    change info command

--- src/cmd/InfoCommand.class.php
+++ src/cmd/InfoCommand.class.php
@@ -17,20 +17,22 @@ class InfoCommand extends BaseCommand {
 
     public function execute(array $args, array $options = array())
     {
+        $return = array();
+
         # get site name
         $site_name = \Bitrix\Main\Config\Option::get("main", "site_name");
 
         $this->info("Information about the current bitrix project:");
-        $site_name = Colors::colorize('Site Name:', Colors::YELLOW)." ".$site_name;
+        $return[] = Colors::colorize('Site Name:', Colors::YELLOW)." ".$site_name;
 
         $MESS = array();
         include_once $_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/lang/ru/interface/epilog_main_admin.php";
         $vendor = COption::GetOptionString("main", "vendor", "1c_bitrix");
         $info_text = $MESS["EPILOG_ADMIN_SM_".$vendor]." (".SM_VERSION.")";
-        $version = Colors::colorize('Version:', Colors::YELLOW)." ".$info_text;
+        $return[] = Colors::colorize('Version:', Colors::YELLOW)." ".$info_text;
 
-        $this->padding($site_name.PHP_EOL.$version);
-
+        # display
+        $this->padding(implode(PHP_EOL,$return));
     }
 
 }
