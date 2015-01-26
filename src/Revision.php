7885916ffb571fc93b01c14c5413d2fa1024eb5e
cjp2600 <cjp2600@ya.ru>
2015-01-26 18:20:36 +0300

    re 21.01.15 add info command

--- src/Revision.php
+++ src/Revision.php
@@ -26,13 +26,13 @@ class Revision {
         } else if ($response['type'] == "error"){
             if (isset($response['error_text'])){
                // throw new \Exception(str_replace("<br>",PHP_EOL,$response['error_text']));
-                $console->writeln(Colors::colorize("     - error : " . $mig, Colors::RED)." ".$this->color("(".str_replace("<br>",PHP_EOL,$response['error_text']).")",Colors::YELLOW));
+                $console->writeln(Colors::colorize("     - error : " . $mig, Colors::RED)." ".Colors::colorize("(".str_replace("<br>",PHP_EOL,$response['error_text']).")",Colors::YELLOW));
                 return false;
             }
         }
 
         if (!$response) {
-            $console->writeln(Colors::colorize("     - error : " . $mig, Colors::RED)." ".$this->color("(Method Up return false)",Colors::YELLOW));
+            $console->writeln(Colors::colorize("     - error : " . $mig, Colors::RED)." ".Colors::colorize("(Method Up return false)",Colors::YELLOW));
         }
 
         return is_bool($response) ? $response : false;
