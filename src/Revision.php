65d1c800b5c8f77abdfde83503ae5e582360e873
cjp2600 <cjp2600@ya.ru>
2015-01-26 18:27:53 +0300

    re 21.01.15 add info command

--- src/Revision.php
+++ src/Revision.php
@@ -25,16 +25,13 @@ class Revision {
             return true;
         } else if ($response['type'] == "error"){
             if (isset($response['error_text'])){
-               // throw new \Exception(str_replace("<br>",PHP_EOL,$response['error_text']));
                 $console->writeln(Colors::colorize("     - error : " . $mig, Colors::RED)." ".Colors::colorize("(".str_replace("<br>",", ",$response['error_text']).")",Colors::YELLOW));
                 return false;
             }
         }
-
         if (!$response) {
             $console->writeln(Colors::colorize("     - error : " . $mig, Colors::RED)." ".Colors::colorize("(Method Up return false)",Colors::YELLOW));
         }
-
         return is_bool($response) ? $response : false;
     }
 
